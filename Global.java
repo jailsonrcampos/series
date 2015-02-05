390b53e0a48df16c6979a1428fc2e18e769cbaa3
jailsonrcampos <jailsonrcampos@gmail.com>
2015-02-05 15:41:12 -0300

    Tentando solucionar erro ao importar series no heroku

--- Global.java
+++ Global.java
@@ -34,7 +34,8 @@ public class Global extends GlobalSettings {
 
     public void iniciaBanco() throws Exception{
 
-        String csvFile = Play.application().getFile("/app/seriesFinalFile.csv").getAbsolutePath();
+        // String csvFile = Play.application().getFile("seriesFinalFile.csv").getAbsolutePath();
+        String csvFile = "seriesFinalFile.csv";
         BufferedReader br = null;
         String line = "";
 
