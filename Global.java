dfbcab227c165dfa94e2aef7e558784eca250ec2
jailsonrcampos <jailsonrcampos@gmail.com>
2015-02-06 02:24:50 -0300

    Update Global.java

--- Global.java
+++ Global.java
@@ -1,23 +1,24 @@
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import au.com.bytecode.opencsv.CSVReader;
+import play.*;
+import play.db.jpa.JPA;
 import models.Episodio;
 import models.Serie;
 import models.Temporada;
 import models.dao.GenericDAO;
-import play.Application;
-import play.GlobalSettings;
-import play.Logger;
-import play.Play;
-import play.db.jpa.JPA;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
-
-import static java.lang.Integer.parseInt;
 
 public class Global extends GlobalSettings {
+	
+	private static final GenericDAO dao = new GenericDAO();
 
+<<<<<<< HEAD
+	@Override
+	public void onStart(Application app) {
+		Logger.info("inicializada...");
+=======
     private static GenericDAO dao = new GenericDAO();
 
     @Override
@@ -68,34 +69,65 @@ public class Global extends GlobalSettings {
                 }
 
                 if (serie.getNome().replace("'", "").equals(listFromCSV[0].replace("'", ""))){
+>>>>>>> origin/master
 
-                    if (serie.getTemporadas().size()!=0 && serie.temporadaMaisNova().getNumero()==parseInt(listFromCSV[1])) {
-                        serie.temporadaMaisNova().addEpisodio(episodio);
-                    } else{
-                        temporada = new Temporada(parseInt(listFromCSV[1]),serie);
-                        temporada.addEpisodio(episodio);
-                        serie.addTemporada(temporada);
-                    }
-                } else{
-                    dao.persist(serie);
-                    serie = new Serie(listFromCSV[0].replace("'", ""));
-                    temporada = new Temporada(parseInt(listFromCSV[1]),serie);
-                    temporada.addEpisodio(episodio);
-                    serie.addTemporada(temporada);
-                }
-            }
-            dao.persist(serie);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+		JPA.withTransaction(new play.libs.F.Callback0() {
+			
+			public void invoke() throws Throwable {
+				
+				List<Serie> seriesDB = dao.findAllByClass(Serie.class);
+				if(seriesDB.size() == 0) {
+					
+					CSVReader reader = new CSVReader(new FileReader("seriesFinalFile.csv"));
+			        String [] nextLine;
+			        
+			        List<Serie> series = new ArrayList<Serie>();
+			        int temporadaAtual = 1;
+			        
+			        while ((nextLine = reader.readNext()) != null) {
+			        	String nomeEpisodio = nextLine[3].equals("") ? "Sem Título" : nextLine[3];			        	
+			        	Serie serie  = new Serie(nextLine[0]);
+			        	int i = series.indexOf(serie);
+			        	if(i >= 0) {
+			        		if(Integer.parseInt(nextLine[1]) == temporadaAtual) {
+			        			Temporada temporada = series.get(i).getTemporadas().get(temporadaAtual-1);
+			        			Episodio episodio = new Episodio(nomeEpisodio, temporada, Integer.parseInt(nextLine[2]));
+			        			temporada.addEpisodio(episodio);
+				                dao.persist(series.get(i));
+				                dao.persist(temporada);
+				                dao.persist(episodio);
+			        		} else {
+			        			Temporada temporada =  new Temporada(Integer.parseInt(nextLine[1]), series.get(i));
+			        			Episodio episodio = new Episodio(nomeEpisodio, temporada, Integer.parseInt(nextLine[2]));
+				                temporada.addEpisodio(episodio);
+				                series.get(i).addTemporada(temporada);
+				                dao.persist(series.get(i));
+				                dao.persist(temporada);
+				                dao.persist(episodio);
+				                temporadaAtual = Integer.parseInt(nextLine[1]); 
+			        		}
+			        	} else {
+			        		Temporada temporada =  new Temporada(Integer.parseInt(nextLine[1]), serie);
+				            Episodio episodio = new Episodio(nomeEpisodio, temporada, Integer.parseInt(nextLine[2]));
+				            temporada.addEpisodio(episodio);
+				            serie.addTemporada(temporada);
+				            dao.persist(serie);
+				            dao.persist(temporada);
+				            dao.persist(episodio);
+			        		series.add(serie);
+			        		temporadaAtual = 1;
+			        	}
+			        }
+			         
+			        reader.close();
+				}
+				
+			}
+		});
+	}
+	
+	public void onStop(Application app) {
+		Logger.info("desligada...");
+	}
 
 }
